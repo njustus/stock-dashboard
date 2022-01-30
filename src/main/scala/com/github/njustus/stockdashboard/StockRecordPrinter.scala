41be45d4ca1b60da3c6de5d74b506e8e1e5eba9f
N. Justus <n-justus@outlook.com>
2022-01-30 17:26:15 +0100

    format currencies using 'NumberFormat'

--- src/main/scala/com/github/njustus/stockdashboard/StockRecordPrinter.scala
+++ src/main/scala/com/github/njustus/stockdashboard/StockRecordPrinter.scala
@@ -4,13 +4,21 @@ import com.github.njustus.stockdashboard.dtos.*
 import cats.effect.IO
 import com.github.njustus.stockdashboard.config.WatchedStock
 
+import java.text.NumberFormat
+import java.util.Formatter.BigDecimalLayoutForm
+import java.util.Locale
+
 object StockRecordPrinter:
+  val format = NumberFormat.getCurrencyInstance(Locale.GERMANY)
+
+  def currency: BigDecimal => String = format.format
+
   def print(record: StockRecord): String =
     val StockRecord(ws, info, exchange) = record
-    s"""${info.name} (${info.wkn}) - ${exchange.price} - ${record.total} EUR (${ws.quantity})"""
+    s"""${info.name} (${info.wkn}) - ${currency(exchange.price)} - ${currency(record.total)} (${ws.quantity})"""
 
   def printRecords(records: List[StockRecord]): String =
     val total = records.map(_.total).sum
     "Name WKN Price Total (Pieces)\n"+
       records.map(print).mkString("\n")+
-    s"\nTotal: ${total} EUR"
+    s"\nTotal: ${currency(total)}"
