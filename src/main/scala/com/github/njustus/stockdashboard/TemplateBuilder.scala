946700d1244adf8126e3f2847839d571a03bcf6a
N. Justus <n-justus@outlook.com>
2022-02-03 17:07:27 +0100

    define custom ResourceTemplateLoader that reads templates from resources

--- src/main/scala/com/github/njustus/stockdashboard/TemplateBuilder.scala
+++ src/main/scala/com/github/njustus/stockdashboard/TemplateBuilder.scala
@@ -1,18 +1,39 @@
 package com.github.njustus.stockdashboard
 
+import com.typesafe.scalalogging.LazyLogging
 import de.neuland.pug4j.{Pug4J, PugConfiguration}
-import de.neuland.pug4j.template.ClasspathTemplateLoader
+import de.neuland.pug4j.template.TemplateLoader
+
 import scala.jdk.CollectionConverters.*
 
+private class ResourceTemplateLoader(resourceBasePath: String) extends TemplateLoader with LazyLogging {
+  import java.io.{InputStreamReader, Reader}
+
+  override def getLastModified(name: String): Long = -1
+
+  override def getReader(name: String): Reader =
+    val path = s"""$resourceBasePath/$name"""
+    logger.debug(s"resolving template $name to $path")
+
+    val is = getClass.getClassLoader.getResourceAsStream(path)
+    assume(is != null, "InputStream from $path is null")
+    InputStreamReader(is)
+
+  override def getExtension: String = "pug"
+
+  override def getBase: String = ""
+}
+
+
 class TemplateBuilder {
-  private val loader = new ClasspathTemplateLoader("views")
-  private val config = new PugConfiguration()
+  private val loader = ResourceTemplateLoader("views")
+  private val config = PugConfiguration()
   config.setCaching(false)
   config.setTemplateLoader(loader)
   config.setPrettyPrint(true)
   config.setMode(Pug4J.Mode.XHTML)
 
   def render(templateName: String, args: Map[String, AnyRef]): String =
-    val template = config.getTemplate(templateName)
+    val template = config.getTemplate(templateName+"."+loader.getExtension)
     config.renderTemplate(template, args.asJava)
 }
